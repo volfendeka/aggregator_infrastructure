86774be533b646acd4db5923ab5abae0151705fc
vdeka <vdeka@etech360.com>
2019-11-02 16:26:16 -0500

    deployment version 5 domain name

--- client/config-prod.js
+++ client/config-prod.js
@@ -1,3 +1,3 @@
-export const baseUrl = "http://rssaggregator.website/api/";
+export const baseUrl = "http://dev.rssaggregator.website/api/";
 export const logoBaseUrl = "../images/logo/";//"https://imagesspace.fra1.digitaloceanspaces.com/news/logo/";
 
