8874f7f247fd26a08b27fa561de4c641ec6bc75b
vdeka <vdeka@etech360.com>
2019-11-02 14:56:26 -0500

    deployment version 1

--- client/config-prod.js
+++ client/config-prod.js
@@ -1,3 +1,3 @@
-export const baseUrl = "http://localhost:82/api/";
+export const baseUrl = "http://dev.rss.aggregator/api/";
 export const logoBaseUrl = "../images/logo/";//"https://imagesspace.fra1.digitaloceanspaces.com/news/logo/";
 
